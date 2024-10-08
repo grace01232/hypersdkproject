edc7932e742f33f6fe4dd5ba0b9b508875f1c173
grace01232 <morgannaomi580@gmail.com>
2024-10-08 15:51:04 +0100

    Update registry.go
    Create and mint asset are added

--- registry/registry.go
+++ registry/registry.go
@@ -25,7 +25,9 @@ func init() {
 		consts.ActionRegistry.Register(&actions.Transfer{}, actions.UnmarshalTransfer, false),
 
 		// TODO: register action: actions.CreateAsset
+		consts.ActionRegistry.Register(&actions.CreateAsset{}, actions.UnmarshalTransfer, false),
 		// TODO: register action: actions.MintAsset
+		consts.ActionRegistry.Register(&actions.MintAsset{}, actions.UnmarshalTransfer, false),
 		consts.ActionRegistry.Register(&actions.BurnAsset{}, actions.UnmarshalBurnAsset, false),
 		consts.ActionRegistry.Register(&actions.ModifyAsset{}, actions.UnmarshalModifyAsset, false),
 
